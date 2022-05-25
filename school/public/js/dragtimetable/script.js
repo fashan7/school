f55783e1d57354fbde7433b0b59f5d9d5586ae60
Fashaan <fashan@datafields.net>
2022-05-25 13:27:05 +0200

    ---

---
+++ school/public/js/dragtimetable/script.js
@@ -0,0 +1,85 @@
+/*jslint white: true, browser: true, undef: true, nomen: true, eqeqeq: true, plusplus: false, bitwise: true, regexp: true, strict: true, newcap: true, immed: true, maxerr: 14 */
+/*global window: false, REDIPS: true */
+
+/* enable strict mode */
+"use strict";
+
+
+// create redips container
+var redips = {};
+
+
+// redips initialization
+redips.init = function () {
+	// reference to the REDIPS.drag object
+	var	rd = REDIPS.drag;
+	// REDIPS.drag initialization
+	rd.init();
+	rd.dropMode = 'single';			// dragged elements can be placed to the empty cells only
+	rd.hover.colorTd = '#9BB3DA';	// set hover color
+	// save - after element is dropped
+	rd.event.dropped = function () {
+		// get element position (method returns array with current and source positions - tableIndex, rowIndex and cellIndex)
+		var pos = rd.getPosition();
+        var data = $('#timetablesubmit').serialize();
+        var term = $('#term').val();
+        var year = $('#year').val();
+        var classno = $('#classno').val();
+        var pso = rd.obj.id + '_' + pos.join('_');
+        var timetableno = $('#timetableno').val();
+        var dataa = [$('#firstperiodst').val(), $('#periodduration').val(), $('#intervalduration').val(), $('#intervalperiod').val(), $('#noofperiod').val()];
+		// save DIV element (AJAX handler is not needed)
+//		rd.ajaxCall('TimetableRegister?p=' + rd.obj.id + '_' + pos.join('_')+'&=term' + term +'&year=' + year + '&gradenumber=' + gradenumber + '&gradesection=' + gradesection);
+        
+        $.ajax({
+            url: 'TimetableRegister',
+            method: 'POST',
+            data: {p: pso, term : term, year : year, classno : classno, timetableno: timetableno, dataa:dataa},
+            success:function(jsonData)
+            {
+                return true;
+            }
+        });
+	};
+	// delete - after element is deleted	
+    rd.event.deleted = function () {
+		// get element position
+		var pos = rd.getPosition(),
+			row = pos[4],
+			col = pos[5];
+		// delete element (AJAX handler is not needed) rd.ajaxCall('ajax/db_delete.php?p=' + rd.obj.id + '_' + row + '_' + col);
+        var delid = rd.obj.id + '_' + row + '_' + col;
+        $.ajax({
+            url: 'deleteDragSubject',
+            method: 'POST',
+            data: {p: delid},
+            success:function(jsonData)
+            {
+//                alert(jsonData);
+                return true;
+            }
+        });		
+	};
+	// set error handler for AJAX call
+	rd.error.ajax = function (xhr) {
+		// display error message
+		document.getElementById('message').innerHTML = 'Error: [' + xhr.status + '] ' + xhr.statusText;
+	};
+	// print message to the message line
+	redips.printMessage('AJAX version');
+};
+
+
+// print message
+redips.printMessage = function (message) {
+	document.getElementById('message').innerHTML = message;
+};
+
+
+// add onload event listener
+if (window.addEventListener) {
+	window.addEventListener('load', redips.init, false);
+}
+else if (window.attachEvent) {
+	window.attachEvent('onload', redips.init);
+}
